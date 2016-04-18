63794c44b40cf4b7c63759369f4ead20317fa525
Aekasitt Guruvanich <aekazitt@gmail.com>
2016-04-18 21:32:58 +0800

    Clean-ups. Extract google analytics tokens and redirect via digibytegaming/ads/redirect

--- src/js/controllers/index.js
+++ src/js/controllers/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('copayApp.controllers').controller('indexController', function($rootScope, $scope, $log, $filter, $timeout, $sce, $sceDelegate, pushNotificationsService, lodash, go, profileService, configService, isCordova, rateService, storageService, addressService, gettext, gettextCatalog, amMoment, nodeWebkit, addonManager, feeService, isChromeApp, bwsError, txFormatService, uxLanguage, $state, glideraService, isMobile, addressbookService) {
+angular.module('copayApp.controllers').controller('indexController', function($rootScope, $scope, $log, $filter, $timeout, $sce, pushNotificationsService, lodash, go, profileService, configService, isCordova, rateService, storageService, addressService, gettext, gettextCatalog, amMoment, nodeWebkit, addonManager, feeService, isChromeApp, bwsError, txFormatService, uxLanguage, $state, glideraService, isMobile, addressbookService) {
   var self = this;
   var SOFT_CONFIRMATION_LIMIT = 12;
   self.isCordova = isCordova;
@@ -27,54 +27,7 @@ angular.module('copayApp.controllers').controller('indexController', function($r
 
   self.goHome = function() {
     go.walletHome();
-
-    // var webview = document.getElementById('advert-webview');
-    // if (webview) {
-    //   console.log('Advert WebView created.');
-    //   webview.addEventListener('newwindow', function(e) {
-    //     e.preventDefault();
-    //     if (e.targetUrl !== 'about:blank') {
-    //         // Easy case where the original link or window.open()
-    //         // already contains a target URL.
-    //         chrome.app.window.create(e.targetUrl, {});
-    //     } else {
-    //       // Create an invisible proxy webview to listen to redirect
-    //       // requests from |newWindow| (the window that the guest is
-    //       // trying to open). NOTE: The proxy webview currently has to
-    //       // live somewhere in the DOM, so we append it to the body.
-    //       // This requirement is in the process of being eliminated.
-    //       var proxyWebview = document.createElement('webview');
-    //       document.body.appendChild(proxyWebview);
-    //
-    //       // Listen to onBeforeRequest event (chrome.webRequest API)
-    //       // on proxyWebview in order to intercept newWindow's redirects.
-    //       var onBeforeRequestListener = function(e) {
-    //         // Only consider top-level non-blank redirects.
-    //         if (e.type === "main_frame" && e.url !== 'about:blank') {
-    //           chrome.app.window.create(e.url, {});
-    //           // Don't need proxyWebview anymore.
-    //           document.body.removeChild(proxyWebview);
-    //           // Handled this redirect: cancel further processing.
-    //           return { cancel: true };
-    //         } else {
-    //           // Ignored this redirect: proceed with default processing.
-    //           return { cancel: false };
-    //         }
-    //       };
-    //       proxyWebview.onBeforeRequest.addListener(
-    //         onBeforeRequestListener,
-    //         { urls: [ "*://*/*" ] },
-    //         [ 'blocking' ]
-    //       );
-    //
-    //       // Attach |newWindow| to proxyWebview. From the original
-    //       // webview guest's point of view, the window is now opened
-    //       // and ready to be redirected: when it does so, the redirect
-    //       // will be intercepted by |onBeforeRequestListener|.
-    //       newWindow.attach(proxyWebview);
-    //     }
-    //   });
-    // } else console.log('Advert WebView not found.');
+    self.loadAdvertisement();
   };
 
   self.menu = [{
@@ -187,7 +140,6 @@ angular.module('copayApp.controllers').controller('indexController', function($r
       self.txps = [];
       self.copayers = [];
       self.updateColor();
-      self.loadAdvertisement();
       self.updateTheme();
       self.updateAlias();
       self.setAddressbook();
@@ -683,10 +635,12 @@ angular.module('copayApp.controllers').controller('indexController', function($r
     xhr.responseType = 'application/json';
     xhr.onload = function(e) {
       var data = JSON.parse(this.response);
-      console.log(data);
+      if (!data) return;
       var adverts = document.getElementById('advert-div');
+      var params;
+      if (data.target_url && data.target_url.lastIndexOf('/')+1 < data.target_url.length) params = data.target_url.substring(data.target_url.lastIndexOf('/')+1, data.target_url.length);
       var a = document.createElement('a');
-      a.href= $sce.trustAsResourceUrl('http://digibytegaming.com/ads/redirect?ad_token='+ data.reference_token);
+      a.href= $sce.trustAsResourceUrl('http://digibytegaming.com/ads/redirect?'+ (params ? params + '&' : '') + 'ad_token='+ data.reference_token);
       a.target='_blank';
       var loadImg = new XMLHttpRequest();
       loadImg.open('GET', data.image.url, true);
@@ -1584,5 +1538,4 @@ angular.module('copayApp.controllers').controller('indexController', function($r
       $rootScope.$apply();
     });
   });
-
 });
